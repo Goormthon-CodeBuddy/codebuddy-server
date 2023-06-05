08a0084c5df38b89fade5bc50208472085e3e452
pepper.yang <pepper@pepperyangui-MacBookPro.local>
2023-06-05 22:54:23 +0900

    feat: api 개발

--- src/routes/gptRouter.ts
+++ src/routes/gptRouter.ts
@@ -1,14 +1,68 @@
-import { NextFunction } from 'express';
-import express, { Request, Response } from 'express';
+import { NextFunction } from "express";
+import express, { Request, Response } from "express";
+const service = require("../service/gptService");
+const { BUDDY_MODE } = require("../constant/gpt.constants");
 
 const router = express.Router();
 
-router.get('/api/gpt',
+router.get(
+  "/api/gpt",
   async (req: Request, res: Response, next: NextFunction) => {
     res.json({
-        message: 'Hello from GPT'
-        });
-    }
+      message: "Hello from GPT",
+    });
+  }
 );
+
+router.post("/api/error-solution", async (req, res) => {
+  const { stack = "", prompt, description = "" } = req.body;
+
+  try {
+    const data = await service.codeErrorSolution({
+      code: prompt,
+      description: description,
+      stack: stack,
+      mode: BUDDY_MODE.CODE_ERROR_SOLUTION,
+    });
+
+    return res.json(data);
+  } catch (error) {
+    return error;
+  }
+});
+
+router.post("/api/code-explanation", async (req, res) => {
+  const { stack = "", prompt, description = "" } = req.body;
+
+  try {
+    const data = await service.codeExplanation({
+      code: prompt,
+      description: description,
+      stack: stack,
+      mode: BUDDY_MODE.CODE_EXPLANATION,
+    });
+
+    return res.json(data);
+  } catch (error) {
+    return error;
+  }
+});
+
+router.post("/api/code-refactoring", async (req, res) => {
+  const { stack = "", prompt, description = "" } = req.body;
+
+  try {
+    const data = await service.codeRefactoring({
+      code: prompt,
+      description: description,
+      stack: stack,
+      mode: BUDDY_MODE.CODE_REFACTORING,
+    });
+
+    return res.json(data);
+  } catch (error) {
+    return error;
+  }
+});
 
 export { router as gptRouter };
---
+++ src/service/gptService.ts
@@ -0,0 +1,165 @@
+/* global OPENAI_AI_GOORMEE_KEY */
+const { Configuration, OpenAIApi } = require("openai");
+const { GPT_TEMPLATE } = require("../constant/gpt.constants");
+const modchatMessage = require("../model/chatMessage");
+
+const openAIConfig = new Configuration({
+  apiKey: "<APIKEY>",
+});
+
+const openai = new OpenAIApi(openAIConfig);
+
+const chatGPTCore = async ({
+  code,
+  description,
+  stack,
+  template,
+}: {
+  code: string;
+  description: string;
+  stack: string;
+  template: any;
+}) =>
+  openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    // temperature: 1,
+    // n: 1,
+    // top_p: 0.2,
+    // frequency_penalty: 0.3,
+    // presence_penalty: 0,
+    messages: [{ role: "user", content: template(code, description, stack) }],
+  });
+
+// 코드 오류 해결
+exports.codeErrorSolution = async ({
+  code,
+  description,
+  stack,
+}: {
+  code: string;
+  description: string;
+  stack: string;
+}) => {
+  try {
+    // openai API 에 코드와 질의 템플릿 보내기
+    const { data } = await chatGPTCore({
+      code,
+      description,
+      stack,
+      template: GPT_TEMPLATE.CODE_ERROR_SOLUTION,
+    });
+
+    // 가공되지 않은 답변
+    const rawAnswer = data?.choices[0]?.message.content;
+
+    // 설명 및 추천코드 분리 가공
+    let details = "";
+    let fixedCode = "";
+
+    // 각 태그로 감싸져 있는 부분 추출
+    details = rawAnswer
+      .substring(
+        rawAnswer.indexOf("<DETAILS>") + 9,
+        rawAnswer.indexOf("</DETAILS>")
+      )
+      .trim();
+    fixedCode = rawAnswer
+      .substring(
+        rawAnswer.indexOf("<FIXED_CODE>") + 12,
+        rawAnswer.indexOf("</FIXED_CODE>")
+      )
+      .trim();
+
+    return {
+      details,
+      fixedCode,
+    };
+  } catch (e) {
+    return e;
+  }
+};
+
+//코드 설명
+exports.codeExplanation = async ({
+  code,
+  description,
+  stack,
+}: {
+  code: string;
+  description: string;
+  stack: string;
+}) => {
+  try {
+    // openai API 에 코드와 질의 템플릿 보내기
+    const { data } = await chatGPTCore({
+      code,
+      description,
+      stack,
+      template: GPT_TEMPLATE.CODE_EXPLANATION,
+    });
+
+    const rawAnswer = data?.choices[0]?.message.content;
+
+    let details = "";
+
+    details = rawAnswer
+      .substring(
+        rawAnswer.indexOf("<DETAILS>") + 9,
+        rawAnswer.indexOf("</DETAILS>")
+      )
+      .trim();
+
+    return details;
+  } catch (error) {
+    return error;
+  }
+};
+
+//코드 개선
+exports.codeRefactoring = async ({
+  code,
+  description,
+  stack,
+}: {
+  code: string;
+  description: string;
+  stack: string;
+}) => {
+  try {
+    // openai API 에 코드와 질의 템플릿 보내기
+    const { data } = await chatGPTCore({
+      code,
+      description,
+      stack,
+      template: GPT_TEMPLATE.CODE_ERROR_SOLUTION,
+    });
+
+    // 가공되지 않은 답변
+    const rawAnswer = data?.choices[0]?.message.content;
+
+    // 설명 및 추천코드 분리 가공
+    let details = "";
+    let fixedCode = "";
+
+    // 각 태그로 감싸져 있는 부분 추출
+    details = rawAnswer
+      .substring(
+        rawAnswer.indexOf("<DETAILS>") + 9,
+        rawAnswer.indexOf("</DETAILS>")
+      )
+      .trim();
+    fixedCode = rawAnswer
+      .substring(
+        rawAnswer.indexOf("<FIXED_CODE>") + 12,
+        rawAnswer.indexOf("</FIXED_CODE>")
+      )
+      .trim();
+
+    return {
+      details,
+      fixedCode,
+    };
+  } catch (e) {
+    return e;
+  }
+};
