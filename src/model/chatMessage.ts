336ce041b60ed20bc7f52ca4c57b9b6e09855c09
pepper.yang <pepper@pepperyangui-MacBookPro.local>
2023-06-06 02:50:58 +0900

    feat: db 수정

--- src/model/chatMessage.ts
+++ src/model/chatMessage.ts
@@ -2,6 +2,22 @@ const chatMessageModel = require("./chatMessageInfo");
 const messagedb = chatMessageModel.get_db();
 
 module.exports = {
-  createMessage: async (query: any) =>
-    await messagedb.chatMessage.create({ ...query, created_at: new Date() }),
+  createMessage: async (message: any) =>
+    await messagedb.chatMessage.create({
+      ...message,
+      is_answer: false,
+      created_at: new Date(),
+    }),
+
+  saveAnswer: async (answer: any) =>
+    await messagedb.chatMessage.create({
+      ...answer,
+      is_answer: true,
+      created_at: new Date(),
+    }),
+
+  getHistory: async (room_index: any) =>
+    await messagedb.chatMessage.get({
+      room_index,
+    }),
 };
--- src/model/chatMessageInfo.ts
+++ src/model/chatMessageInfo.ts
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
+const AutoIncrement1 = require("mongoose-sequence")(mongoose);
 
 const chatMessageInfoFields = {
-  index: { type: Number, trim: true, required: true },
+  m_index: {
+    type: Number,
+    trim: true,
+    unique: true,
+  },
   room_index: { type: Number, trim: true, required: true },
   template_index: { type: Number, trim: true },
   content: { type: Object },
@@ -12,6 +17,8 @@ const chatMessageInfoFields = {
 const chatMessageInfoSchema = new mongoose.Schema(chatMessageInfoFields, {
   bufferCommands: true,
 });
+
+chatMessageInfoSchema.plugin(AutoIncrement1, { inc_field: "m_index" });
 
 const chatMessageDB = {
   chatMessage: mongoose.model("chat_messages", chatMessageInfoSchema),
