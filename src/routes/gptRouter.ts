c8515b0fab08339cee941c8b0fff7054a9938849
goorm <goorm@SummerBae.local>
2023-06-05 17:01:19 +0900

    feat(notification): chatgpt api 답변 완료 알림

--- src/routes/gptRouter.ts
+++ src/routes/gptRouter.ts
@@ -3,12 +3,16 @@ import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.get('/api/gpt',
+router.get('/api/chatgpt',
   async (req: Request, res: Response, next: NextFunction) => {
     res.json({
         message: 'Hello from GPT'
         });
     }
 );
+
+router.post('/api/chatgpt/notification/arrive', async (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send('ChatGPT 답변이 완료되었습니다.');
+});
 
 export { router as gptRouter };
